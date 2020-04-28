e397ff075e628343535e1c3efb0eb3efb26da28b
lewis <lewis@gmail.com>
2020-04-28 17:53:41 +0100

    remove phone field

---
+++ src/views/Auth/components/signup.test.tsx
@@ -0,0 +1,24 @@
+// import React, { useState, useContext, useEffect } from "react";
+import { render } from "@testing-library/react";
+// import firebase from "../../../firebase";
+// import "firebase/auth";
+// import faker from "faker";
+// import Signup from '../components/signup';
+
+describe("Sign up user", () => {
+  test("User can sign up", async () => {
+    console.log("lorem");
+  });
+
+  test("Create user db entry", async () => {
+    console.log("lorem");
+  });
+
+  test("Existing user can't sign up", async () => {
+    console.log("lorem");
+  });
+
+  test("Weak password fails", async () => {
+    console.log("lorem");
+  });
+});
