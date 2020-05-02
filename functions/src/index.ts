7944005c3e172bdabea554e9eae4f9d3cba62338
lewis <lewis@gmail.com>
2020-05-02 18:56:37 +0100

    functions setup

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,10 @@
-const checkUsername = require("./check-username");
+import * as functions from "firebase-functions";
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-module.exports = {
-  "check-username": functions.https.onCall(checkUsername),
-};
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
