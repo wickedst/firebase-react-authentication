b7f976cc7f60e948c91ff40fe93f01dbdd795b3f
Lewis <l.tyler@gmail.com>
2020-05-07 19:34:50 +0100

    start on cloud storage thumbnails

--- src/views/Home/components/home.tsx
+++ src/views/Home/components/home.tsx
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import firebase from "firebase";
+import { AuthContext } from "../../../AuthProvider";
+
+function displayImage(imageRef: firebase.storage.Reference): void {
+  imageRef
+    .getDownloadURL()
+    .then(function (url) {
+      console.log(url);
+      // TODO: Display the image on the UI
+    })
+    .catch(function (error) {
+      // Handle any errors
+    });
+}
 const Home = () => {
+  const { userProfile } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (userProfile) {
+      const storageRef = firebase.storage().ref(`users/${userProfile.uid}/`);
+
+      storageRef
+        .listAll()
+        .then(function (result) {
+          result.items.forEach(function (imageRef) {
+            console.log("imageRef ", imageRef);
+            displayImage(imageRef);
+          });
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }, [userProfile]);
   return (
     <div className="text-center container py-4">
       <h1>Homepage</h1>
